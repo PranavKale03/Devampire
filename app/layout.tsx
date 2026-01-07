09fadd639351b0292d1ff87b13eabb4c36fa56c1
Pranav Kale <pranavmkale99@gmail.com>
2026-01-07 19:17:15 +0530

    Build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
 import Navbar from "../components/Navbar";
 import "./globals.css";
-
-const openSans = Open_Sans({
-  subsets: ["latin"],
-  variable: "--font-open-sans",
-});
 
 export const metadata: Metadata = {
   title: "Devampire",
@@ -20,8 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
+      </head>
       <body
-        className={`${openSans.className} antialiased pt-12`}
+        className={`antialiased pt-12 font-[family-name:Open_Sans,sans-serif]`}
       >
         <Navbar />
         {children}
