3113283ef0e39b444fefa7c36cc1d840ab73cb28
Pranav Kale <pranavmkale99@gmail.com>
2026-01-07 19:09:19 +0530

    Env added

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://devampire-server.onrender.com' });
+const API = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 
 API.interceptors.request.use((req) => {
     if (typeof window !== 'undefined' && localStorage.getItem('Profile')) {
